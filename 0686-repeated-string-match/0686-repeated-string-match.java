32d28638b4d6d00d2620299241a5fe9ae50ad7cc
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-02-28 16:34:19 +0530

    Time: 11 ms (90.99%), Space: 43.9 MB (37.62%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -14,12 +14,61 @@ class Solution {
         if(isSubstring(sa.toString(),b)) return repeat;
         return -1;
     }
-    boolean isSubstring(String a, String b){
-        for(int i=0;i<=a.length()-b.length();i++){
-            int j=0;
-            while(j<b.length() && a.charAt(i+j)==b.charAt(j)) j++;
-            if(j==b.length()) return true;
+    // boolean isSubstring(String a, String b){
+    //     for(int i=0;i<=a.length()-b.length();i++){
+    //         int j=0;
+    //         while(j<b.length() && a.charAt(i+j)==b.charAt(j)) j++;
+    //         if(j==b.length()) return true;
+    //     }
+    //     return false;
+    // }
+    boolean isSubstring(String text, String pattern) {
+    if(pattern.length() == 0) return true;
+
+    int[] lps = buildLPS(pattern);
+
+    int i = 0; // pointer for text
+    int j = 0; // pointer for pattern
+
+    while(i < text.length()){
+        if(text.charAt(i) == pattern.charAt(j)){
+            i++;
+            j++;
+
+            if(j == pattern.length()) return true;
+        } 
+        else {
+            if(j != 0){
+                j = lps[j - 1]; // ⭐ KMP magic
+            } else {
+                i++;
+            }
         }
-        return false;
     }
+    return false;
+}
+    int[] buildLPS(String pattern){
+    int n = pattern.length();
+    int[] lps = new int[n];
+
+    int len = 0; // length of previous longest prefix suffix
+    int i = 1;
+
+    while(i < n){
+        if(pattern.charAt(i) == pattern.charAt(len)){
+            len++;
+            lps[i] = len;
+            i++;
+        } 
+        else {
+            if(len != 0){
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
 }
