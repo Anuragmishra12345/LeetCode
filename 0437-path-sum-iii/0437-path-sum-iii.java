e69d34bd75cb673004e314f33fa29497f407565c
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-03 02:40:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -16,17 +16,33 @@
 class Solution {
     int ans=0;
     public int pathSum(TreeNode root, int targetSum) {
-       if(root==null) return 0;
-       dfs(root,targetSum);
-       pathSum(root.left,targetSum);
-       pathSum(root.right,targetSum);
+    //    if(root==null) return 0;
+    //    dfs(root,targetSum);
+    //    pathSum(root.left,targetSum);
+    //    pathSum(root.right,targetSum);
+        Map<Long,Integer> map=new HashMap<>();
+        map.put(0L,1);
+        dfs(root,0,targetSum,map);
        return ans;
     }
-    void dfs(TreeNode root, long target){
-        if(root==null) return ;
-        if(target==root.val) ans++;
+    // void dfs(TreeNode root, long target){
+    //     if(root==null) return ;
+    //     if(target==root.val) ans++;
 
-        dfs(root.left,target-root.val);
-        dfs(root.right,target-root.val);
+    //     dfs(root.left,target-root.val);
+    //     dfs(root.right,target-root.val);
+    // }
+
+    void dfs(TreeNode root, long currSum, int target, Map<Long,Integer> map){
+        if(root==null) return;
+        currSum+=root.val;
+        ans+=map.getOrDefault(currSum-target,0);
+
+        map.put(currSum,map.getOrDefault(currSum,0)+1);
+
+        dfs(root.left, currSum, target, map);
+        dfs(root.right, currSum, target, map);
+
+        map.put(currSum,map.get(currSum)-1);
     }
 }
