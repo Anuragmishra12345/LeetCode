9c306e25b17f1accf5867636af8e5b9180e8bd16
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-04 19:48:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -28,30 +28,21 @@ class Solution {
     }
     private void dfs(Node root){
         if(root==null) return;
-        Set<Node> set=new HashSet<>();
         Queue<Node> queue=new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()){
             int size=queue.size();
-            Node curr=queue.poll();
-            if(!set.contains(curr)){
-                set.add(curr);
-                if(curr.left!=null){
-                    queue.add(curr.left);
-                    queue.add(curr.right);
+            Node prev=null;
+            
+            for(int i=0;i<size;i++){
+                Node curr=queue.poll();
+                if(prev!=null){
+                    prev.next=curr;
                 }
-            }
-            for(int i=1;i<size;i++){
-                Node next=queue.poll();
-                curr.next=next;
-                curr=next;
-                if(!set.contains(next)){
-                set.add(next);
-                if(next.left!=null){
-                    queue.add(next.left);
-                    queue.add(next.right);
-                }
-            }
+                prev=curr;
+            
+                if(curr.left != null) queue.add(curr.left);
+                if(curr.right != null) queue.add(curr.right);
             }
         }
     }
