9e1814e332d640833ebd6ebae354b5b8218eba7b
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-06 20:01:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,22 +2,22 @@ class Solution {
     int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
     int m,n;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc]==color) return image;
         m=image.length;
         n=image[0].length;
-        boolean[][] visited=new boolean[m][n];
+
         int changeColor=image[sr][sc];
-        dfs(image,sr,sc,changeColor,color,visited);
+        dfs(image,sr,sc,changeColor,color);
         return image;
     }
-    void dfs(int[][] image, int sr, int sc,int cc, int color, boolean[][] visited){
+    void dfs(int[][] image, int sr, int sc,int cc, int color){
         image[sr][sc]=color;
-        visited[sr][sc]=true;
         
         for(int[] d:dirs){
             int nr=sr+d[0];
             int nc=sc+d[1];
-            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && image[nr][nc]==cc)
-            dfs(image,nr,nc,cc,color,visited);
+            if(nr>=0 && nc>=0 && nr<m && nc<n && image[nr][nc]==cc)
+            dfs(image,nr,nc,cc,color);
         }
     }
 }
