7c6ea4b7215bb3b18c597d56fa908f82dd398dc3
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-16 12:26:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,37 +1,21 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        // Map<Integer,Set<Integer>> map=new HashMap<>();
-        // for(int stone: stones) map.put(stone, new HashSet<>());
-
-        // map.get(0).add(0);
-
-        // for(int stone:stones){
-        //     for(int k:map.get(stone)){
-        //         for(int jump=k-1;jump<=k+1;jump++){
-        //             if(jump>0 && map.containsKey(stone+jump)){
-        //                 map.get(stone+jump).add(jump);
-        //             }
-        //         }
-        //     }
-        // }
-        // return !map.get(stones[stones.length-1]).isEmpty();
-
-
+        if(stones[1] != 1) return false;
         Map<Integer,Set<Integer>> map=new HashMap<>();
-        for(int stone:stones) map.put(stone,new HashSet<>());
+        for(int stone: stones) map.put(stone, new HashSet<>());
 
         map.get(0).add(0);
 
         for(int stone:stones){
             for(int k:map.get(stone)){
                 for(int jump=k-1;jump<=k+1;jump++){
-                    if(jump>0 && map.containsKey(jump+stone)){
-                        map.get(jump+stone).add(jump);
+                    if(jump>0 && map.containsKey(stone+jump)){
+                        map.get(stone+jump).add(jump);
                     }
                 }
             }
         }
-
         return !map.get(stones[stones.length-1]).isEmpty();
+
     }
 }
