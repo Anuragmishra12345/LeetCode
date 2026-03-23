b657c0c3c7f2b6eb56b84ce7c9aa20d3a76c479a
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-24 00:44:14 +0530

    Time: 5 ms (99.88%), Space: 43.2 MB (75.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -43,4 +43,17 @@ class Solution {
     //     }
     //     return prev[amount];
     // }
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int t = coin; t <= amount; t++) {
+                dp[t] += dp[t - coin];
+            }
+        }
+
+        return dp[amount];
+    }
 }
