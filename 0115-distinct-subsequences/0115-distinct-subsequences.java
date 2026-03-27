7ef6738e13e7a1a1c01cbce7177bc038a5533dbd
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-27 15:58:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,16 +4,25 @@ class Solution {
         int n=s.length();
         int m=t.length();
         dp=new int[n+1][m+1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return f(n-1, m-1, s,t);
-    }
-    int f(int i , int j , String a, String b){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        // for (int[] row : dp) Arrays.fill(row, -1);
+        // return f(n-1, m-1, s,t);
+        for(int i=0;i<=n;i++) dp[i][0]=1;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(a.charAt(i)==b.charAt(j)) return dp[i][j]= f(i-1, j-1, a, b)+f(i-1,j,a,b);
-        else return dp[i][j]=f(i-1,j,a,b);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }
+    // int f(int i , int j , String a, String b){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+
+    //     if(dp[i][j]!=-1) return dp[i][j];
+
+    //     if(a.charAt(i)==b.charAt(j)) return dp[i][j]= f(i-1, j-1, a, b)+f(i-1,j,a,b);
+    //     else return dp[i][j]=f(i-1,j,a,b);
+    // }
 }
