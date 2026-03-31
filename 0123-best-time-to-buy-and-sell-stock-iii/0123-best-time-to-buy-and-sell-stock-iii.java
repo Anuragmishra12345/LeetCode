f2f5d59b75e62a89cb5d2c87cec4eea0d0506b8b
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-03-31 11:06:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,45 @@
 class Solution {
-    int[][][] dp;
+    // int[][][] dp;
+    // public int maxProfit(int[] prices) {
+    //     int n=prices.length;
+    //     dp=new int[n][2][3];
+    //     for(int[][] table:dp) {
+    //         for(int[] row: table ) Arrays.fill(row,-1);
+    //     }
+    //     return f(prices,0,1,2);
+    // }
+    // int f(int[] prices, int index, int status, int t){
+    //     if(t==0 || index==prices.length){
+    //         return 0;
+    //     }
+    //     if(dp[index][status][t]!=-1) return dp[index][status][t];
+    //     long profit=0;
+    //     if(status==1){
+    //         profit=Math.max(-prices[index]+f(prices,index+1,0,t),f(prices,index+1,1,t));
+    //     }
+    //     else {
+    //         profit=Math.max(prices[index]+f(prices,index+1,1,t-1),f(prices,index+1,0,t));
+    //     }
+
+    //     return dp[index][status][t]=(int)profit;
+    // }
+
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        dp=new int[n][2][3];
-        for(int[][] table:dp) {
-            for(int[] row: table ) Arrays.fill(row,-1);
-        }
-        return f(prices,0,1,2);
-    }
-    int f(int[] prices, int index, int status, int t){
-        if(t==0 || index==prices.length){
-            return 0;
-        }
-        if(dp[index][status][t]!=-1) return dp[index][status][t];
-        long profit=0;
-        if(status==1){
-            profit=Math.max(-prices[index]+f(prices,index+1,0,t),f(prices,index+1,1,t));
-        }
-        else {
-            profit=Math.max(prices[index]+f(prices,index+1,1,t-1),f(prices,index+1,0,t));
-        }
+        int[][][] dp=new int[n+1][2][3];
 
-        return dp[index][status][t]=(int)profit;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int t=1;t<=2;t++){
+                    if(j==1){
+                        dp[i][j][t]=Math.max(-prices[i]+dp[i+1][0][t], dp[i+1][1][t]);
+                    }
+                    else {
+                        dp[i][j][t]=Math.max(prices[i]+dp[i+1][1][t-1], dp[i+1][0][t]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 }
