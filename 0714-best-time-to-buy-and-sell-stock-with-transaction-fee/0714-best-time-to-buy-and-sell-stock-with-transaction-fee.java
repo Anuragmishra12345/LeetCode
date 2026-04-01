82ac3466db46116dd21a3d310e17a0c7af094bfe
Anuragmishra12345 <mishraanurag06542@gmail.com>
2026-04-01 22:42:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,26 +1,43 @@
 class Solution {
-    int[][] dp;
-    int n;
+    // int[][] dp;
+    // int n;
+    // public int maxProfit(int[] prices, int fee) {
+    //     n=prices.length;
+    //     dp=new int[n][2];
+    //     for(int [] row:dp) Arrays.fill(row,-1);
+    //     return f(prices,0,1,fee);
+    // }
+    // int f(int[] prices, int index, int status,int fee){
+    //     if(index==n) return 0;
+    //     long profit=0;
+
+    //     if(dp[index][status]!=-1) return dp[index][status];
+
+    //     if(status==1){
+    //         profit=Math.max(-fee-prices[index]+f(prices,index+1,0,fee),
+    //                 0+f(prices,index+1,1,fee));
+    //     }
+    //     else {
+    //         profit=Math.max(prices[index]+f(prices,index+1,1,fee),
+    //                         0+f(prices,index+1,0,fee));
+    //     }
+    //     return dp[index][status]=(int)profit;
+    // } 
+
+
+// Greedy
     public int maxProfit(int[] prices, int fee) {
-        n=prices.length;
-        dp=new int[n][2];
-        for(int [] row:dp) Arrays.fill(row,-1);
-        return f(prices,0,1,fee);
+        int hold = -prices[0]; // buy first stock
+        int cash = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            cash = Math.max(cash, hold + prices[i] - fee); // sell
+
+            hold = Math.max(hold, cash - prices[i]);       // buy
+        }
+
+        return cash;
     }
-    int f(int[] prices, int index, int status,int fee){
-        if(index==n) return 0;
-        long profit=0;
 
-        if(dp[index][status]!=-1) return dp[index][status];
-
-        if(status==1){
-            profit=Math.max(-fee-prices[index]+f(prices,index+1,0,fee),
-                    0+f(prices,index+1,1,fee));
-        }
-        else {
-            profit=Math.max(prices[index]+f(prices,index+1,1,fee),
-                            0+f(prices,index+1,0,fee));
-        }
-        return dp[index][status]=(int)profit;
-    } 
 }
