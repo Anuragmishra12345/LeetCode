498fb13019ca0f65dd620aff0a366ab713d967e5
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-12 15:45:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -41,18 +41,31 @@
 //     }
 // }
 
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int[][] dp=new int[n+1][n+1];
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=i-1;j>=-1;j--){
+//                 int len=dp[i+1][j+1];
+//                 if(j==-1 || nums[i]>nums[j]) len=Math.max(len,1+dp[i+1][i+1]);
+//                 dp[i][j+1]=len;
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[][] dp=new int[n+1][n+1];
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                int len=dp[i+1][j+1];
-                if(j==-1 || nums[i]>nums[j]) len=Math.max(len,1+dp[i+1][i+1]);
-                dp[i][j+1]=len;
-                
+        int n=nums.length; 
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]) dp[i]=Math.max(dp[i],1+dp[j]);
             }
         }
-        return dp[0][0];
+        return dp[n-1];
     }
 }
