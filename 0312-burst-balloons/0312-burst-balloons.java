74d56b5623ef6943c125c07f17daf247cf6e648e
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-15 12:00:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,25 +1,48 @@
 class Solution {
-    int[][] dp;
+    // int[][] dp;
+    // public int maxCoins(int[] nums) {
+    //     int n=nums.length;
+    //     dp=new int[n+2][n+2];
+    //     for(int[] row:dp) Arrays.fill(row,-1);
+    //     int[] arr=new int[n+2];
+    //     arr[0]=1;
+    //     arr[n+1]=1;
+    //     for(int i=0;i<n;i++){
+    //         arr[i+1]=nums[i];
+    //     }
+    //     return f(1,n,arr);
+    // }
+    // int f(int i, int j, int[] nums){
+    //     if(i>j) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int max=Integer.MIN_VALUE;
+    //     for(int k=i;k<=j;k++){
+    //         int coin=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums)+f(k+1,j,nums);
+    //         max=Math.max(coin,max);
+    //     }
+    //     return dp[i][j]=max;
+    // }
+
     public int maxCoins(int[] nums) {
         int n=nums.length;
-        dp=new int[n+2][n+2];
-        for(int[] row:dp) Arrays.fill(row,-1);
+        int[][] dp=new int[n+2][n+2];
         int[] arr=new int[n+2];
         arr[0]=1;
         arr[n+1]=1;
         for(int i=0;i<n;i++){
             arr[i+1]=nums[i];
         }
-        return f(1,n,arr);
-    }
-    int f(int i, int j, int[] nums){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int max=Integer.MIN_VALUE;
-        for(int k=i;k<=j;k++){
-            int coin=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums)+f(k+1,j,nums);
-            max=Math.max(coin,max);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int max=Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int coin=arr[i-1]*arr[k]*arr[j+1]+dp[i][k-1]+dp[k+1][j];
+                    max=Math.max(coin,max);
+                }
+                dp[i][j]=max;
+            }
         }
-        return dp[i][j]=max;
+        return dp[1][n];
     }
 }
