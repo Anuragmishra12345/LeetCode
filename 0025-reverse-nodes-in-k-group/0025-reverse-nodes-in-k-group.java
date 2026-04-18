0ae7a8e2f0ebcd6be6cecfb545b4fb257a683795
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-18 19:35:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,76 +9,76 @@
  * }
  */
 class Solution {
-    // public ListNode reverseKGroup(ListNode head, int k) {
-    //     if(head==null || k<=1) return head;
-
-    //     ListNode prev=null;
-    //     ListNode current=head;
-    //     while(true){
-    //         ListNode check=current;
-    //         for(int i=0;i<k;i++){
-    //             if(check==null) return head;
-    //             check=check.next;
-    //         }
-    //         ListNode last=prev;
-    //         ListNode newEnd=current;
-
-    //         ListNode next=current.next; 
-    //         for(int i=0;i<k && current!=null;i++){
-    //             current.next=prev;
-    //             prev=current;
-    //             current=next;
-    //             if(next!=null){
-    //                 next=next.next;
-    //             }
-    //         }
-
-    //         if(last!=null){
-    //             last.next=prev;
-    //         }else {
-    //             head=prev;
-    //         }
-
-    //         newEnd.next=current;
-    //         if(current==null){
-    //             break;
-    //         }
-    //         prev=newEnd;
-    //     }
-    //     return head;
-    // } 
-
     public ListNode reverseKGroup(ListNode head, int k) {
         if(head==null || k<=1) return head;
 
         ListNode prev=null;
-        ListNode curr=head;
-
+        ListNode current=head;
         while(true){
-            ListNode check=curr;
+            ListNode check=current;
             for(int i=0;i<k;i++){
                 if(check==null) return head;
                 check=check.next;
             }
-
             ListNode last=prev;
-            ListNode newEnd=curr;
+            ListNode newEnd=current;
 
-            ListNode prevNode = null;
-            for(int i=0;i<k && curr!=null;i++){
-                ListNode next=curr.next;
-                curr.next=prevNode;
-                prevNode=curr;
-                curr=next;
+            ListNode next=current.next; 
+            for(int i=0;i<k && current!=null;i++){
+                current.next=prev;
+                prev=current;
+                current=next;
+                if(next!=null){
+                    next=next.next;
+                }
             }
 
-            if(last!=null) last.next=prevNode;
-            else head=prevNode;
+            if(last!=null){
+                last.next=prev;
+            }else {
+                head=prev;
+            }
 
-            newEnd.next=curr;
+            newEnd.next=current;
+            if(current==null){
+                break;
+            }
             prev=newEnd;
         }
-    }
+        return head;
+    } 
+
+    // public ListNode reverseKGroup(ListNode head, int k) {
+    //     if(head==null || k<=1) return head;
+
+    //     ListNode prev=null;
+    //     ListNode curr=head;
+
+    //     while(true){
+    //         ListNode check=curr;
+    //         for(int i=0;i<k;i++){
+    //             if(check==null) return head;
+    //             check=check.next;
+    //         }
+
+    //         ListNode last=prev;
+    //         ListNode newEnd=curr;
+
+    //         ListNode prevNode = null;
+    //         for(int i=0;i<k && curr!=null;i++){
+    //             ListNode next=curr.next;
+    //             curr.next=prevNode;
+    //             prevNode=curr;
+    //             curr=next;
+    //         }
+
+    //         if(last!=null) last.next=prevNode;
+    //         else head=prevNode;
+
+    //         newEnd.next=curr;
+    //         prev=newEnd;
+    //     }
+    // }
 
 
 }
