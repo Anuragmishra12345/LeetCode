190560b3a200f2c593821d4f0e5b2145f65a1983
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-19 16:31:46 +0530

    Time: 1 ms (100%), Space: 45.5 MB (59.74%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -14,25 +14,68 @@
  * }
  */
 class Solution {
+    List<List<Integer>> result=new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> result=new ArrayList<>();
-        dfs(root,result,new ArrayList<>(), targetSum,0);
+        dfs(root,new ArrayList<>(), targetSum,0);
         return result;
     }
-    void dfs(TreeNode root, List<List<Integer>> result, List<Integer> curr, int target, int sum){
-        if(root==null) return;
+    // void dfs(TreeNode root, List<List<Integer>> result, List<Integer> curr, int target, int sum){
+    //     if(root==null) return;
+    //     sum+=root.val;
+    //     curr.add(root.val);
+    //     if(root.left==null && root.right==null){
+    //         if(sum==target){
+    //             result.add(new ArrayList<>(curr));
+    //         }
+    //         curr.remove(curr.size()-1);
+    //         return;
+    //     }
+    //     dfs(root.left,result,curr,target,sum);
+    //     dfs(root.right,result,curr,target,sum);
+
+    //     curr.remove(curr.size()-1);
+    // }
+
+
+    void dfs(TreeNode root,List<Integer> curr, int target, int sum){
+        if(root==null) return ;
+
         sum+=root.val;
         curr.add(root.val);
+
         if(root.left==null && root.right==null){
-            if(sum==target){
-                result.add(new ArrayList<>(curr));
-            }
+            if(sum==target) result.add(new ArrayList<>(curr));
             curr.remove(curr.size()-1);
             return;
         }
-        dfs(root.left,result,curr,target,sum);
-        dfs(root.right,result,curr,target,sum);
+
+        dfs(root.left,curr,target,sum);
+        dfs(root.right,curr,target,sum);
 
         curr.remove(curr.size()-1);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
