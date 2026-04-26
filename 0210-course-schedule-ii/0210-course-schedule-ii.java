5d61c8904e241cfde250b519bc9b68ce24f198f4
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-26 15:51:07 +0530

    Time: 5 ms (83.83%), Space: 47 MB (45.48%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,45 +1,80 @@
 class Solution {
-    boolean[] visited;
-    boolean[] path;
-    public int[] findOrder(int numCourses, int[][] pr) {
-        visited=new boolean[numCourses];
-        path = new boolean[numCourses];
-        Stack<Integer> stack=new Stack<>();
+    // boolean[] visited;
+    // boolean[] path;
+    // public int[] findOrder(int numCourses, int[][] pr) {
+    //     visited=new boolean[numCourses];
+    //     path = new boolean[numCourses];
+    //     Stack<Integer> stack=new Stack<>();
 
+    //     List<List<Integer>> adj=new ArrayList<>();
+    //     for(int i=0;i<numCourses;i++){
+    //         adj.add(new ArrayList<>());
+    //     }
+    //     for(int[] it:pr){
+    //         adj.get(it[1]).add(it[0]);
+    //     }
+
+    //     for(int i=0;i<numCourses;i++){
+    //         if(!visited[i]) {
+    //             if(dfs(i, adj, stack)) return new int[0]; 
+    //         }
+    //     }
+
+    //     int[] result=new int[numCourses];
+    //     int i=0;
+    //     while(!stack.isEmpty()){
+    //         result[i++]=stack.pop();
+    //     }
+    //     return result;
+    // }
+    // public boolean dfs(int node,List<List<Integer>> adj,Stack<Integer> stack){
+    //     visited[node]=true;
+    //     path[node]=true;
+    //     for(int n:adj.get(node)){
+    //         if(!visited[n]){
+    //             if(dfs(n, adj, stack)) return true;
+    //         }
+    //         else if(path[n]){
+    //             return true; 
+    //         }
+    //     }
+    //     stack.push(node);
+    //     path[node]=false;
+    //     return false;
+    // }
+
+
+    public int[] findOrder(int numCourses, int[][] pr) {
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
+
+        int[] indegree=new int[numCourses];
         for(int[] it:pr){
             adj.get(it[1]).add(it[0]);
+            indegree[it[0]]++;
         }
 
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i]) {
-                if(dfs(i, adj, stack)) return new int[0]; 
-            }
-        }
-
+        Queue<Integer> queue=new LinkedList<>();
         int[] result=new int[numCourses];
-        int i=0;
-        while(!stack.isEmpty()){
-            result[i++]=stack.pop();
+        int idx=0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) queue.offer(i);
         }
+
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            result[idx++]=node;
+
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) queue.offer(it);
+            }
+        }
+
+        if(idx!=numCourses) return new int[0];
+
         return result;
-    }
-    public boolean dfs(int node,List<List<Integer>> adj,Stack<Integer> stack){
-        visited[node]=true;
-        path[node]=true;
-        for(int n:adj.get(node)){
-            if(!visited[n]){
-                if(dfs(n, adj, stack)) return true;
-            }
-            else if(path[n]){
-                return true; 
-            }
-        }
-        stack.push(node);
-        path[node]=false;
-        return false;
     }
 }    
