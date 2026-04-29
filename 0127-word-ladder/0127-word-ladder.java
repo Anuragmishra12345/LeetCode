197d20faf914b9549599201d3bb8076209dda777
Anurag Mishra <mishraanurag06542@gmail.com>
2026-04-29 18:55:41 +0530

    Time: 75 ms (59.72%), Space: 48.9 MB (30.92%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,35 +1,40 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set=new HashSet(wordList);
-        if(!set.contains(endWord)) return 0;
-        Queue<String> q=new LinkedList<>();
-        q.add(beginWord);
-        int step=1;
+        Set<String> set=new HashSet<>();
+        for(int i=0;i<wordList.size();i++){
+            set.add(wordList.get(i));
+        }
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(beginWord,1));
+        set.remove(beginWord);
 
         while(!q.isEmpty()){
-            int size=q.size();
+            String word=q.peek().word;
+            int level=q.peek().level;
+            q.remove();
 
-            for(int i=0;i<size;i++){
-                String s=q.poll();
-                if(s.equals(endWord)) return step;
+            if(word.equals(endWord)) return level;
 
-                char[] arr=s.toCharArray();
-
-                for(int j=0;j<arr.length;j++){
-                    char original=arr[j];
-                    for(char ch='a';ch<='z';ch++){
-                        arr[j]=ch;
-                        String temp=new String(arr);
-                        if(set.contains(temp)){
-                            set.remove(temp);
-                            q.add(temp);
-                        }
+            for(int i=0;i<word.length();i++){
+                char[] wordArray=word.toCharArray();
+                for(char ch='a';ch<='z';ch++){
+                    wordArray[i]=ch;
+                    if(set.contains(new String(wordArray))){
+                        q.add(new Pair(new String(wordArray),level+1));
+                        set.remove(new String(wordArray));
                     }
-                    arr[j]=original;
                 }
             }
-            step++;
         }
         return 0;
+    }
+}
+class Pair{
+    String word;
+    int level;
+
+    Pair(String word, int value){
+        this.word=word;
+        this.level=value;
     }
 }
