3ea875448867e548b6dd0d18824a7f07890610a9
Anurag Mishra <mishraanurag06542@gmail.com>
2026-05-04 19:36:39 +0530

    Time: 19 ms (25.44%), Space: 47 MB (97.55%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    class Tuple{
+        int first;
+        int second;
+        int third;
+
+        Tuple(int first, int second, int third){
+            this.first=first;
+            this.second=second;
+            this.third=third;
+        }
+    }
+    int[][] dir={{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        int[][] path=new int[n][n];
+        for(int[] row:path) Arrays.fill(row,Integer.MAX_VALUE);
+        path[0][0]=1;
+        Queue<Tuple> q=new LinkedList<>();
+        q.add(new Tuple(1,0,0));
+
+        while(!q.isEmpty()){
+            Tuple it=q.poll();
+            int dist=it.first;
+            int r=it.second;
+            int c=it.third;
+
+            for(int i=0;i<8;i++){
+                int nr=r+dir[i][0];
+                int nc=c+dir[i][1];
+                if(dist>path[r][c]) continue;
+                if(nr>=0 && nr<n && nc<n && nc>=0 && grid[nr][nc]==0 && dist+1<path[nr][nc]){
+                    path[nr][nc]=dist+1;
+                    q.add(new Tuple(dist+1,nr,nc));
+                }
+            }
+        }
+        return (path[n-1][n-1]!=Integer.MAX_VALUE)? path[n-1][n-1]:-1;
+    }
+}
