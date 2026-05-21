581319f6fd23257dc1487d7d6e864fad5905ef4e
Anurag Mishra <mishraanurag06542@gmail.com>
2026-05-21 12:33:39 +0530

    Time: 2 ms (46.88%), Space: 44.4 MB (10.9%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,14 +1,53 @@
 import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        // int carry=0;
-        // StringBuilder sb=new StringBuilder();
-        // int lena=a.length()-1;
-        // int lenb=b.length()-1;
-        BigInteger x=new BigInteger (a,2);
-        BigInteger y=new BigInteger (b,2);
-        BigInteger c=x.add(y);
-        return c.toString(2);
-        // while()
+        int carry=0;
+        StringBuilder sb=new StringBuilder();
+        int lena=a.length()-1;
+        int lenb=b.length()-1;
+        
+        while(lena>=0 && lenb>=0){
+            int sum=carry+a.charAt(lena)-'0'+b.charAt(lenb)-'0';
+            if(sum==3){
+                carry=1;
+                sb.insert(0,1);
+            }
+            else if(sum==2){
+                carry=1;
+                sb.insert(0,0);
+            }
+            else {
+                carry=0;
+                sb.insert(0,sum);
+            }
+            lena--;
+            lenb--;
+        }
+        while(lena>=0){
+            int sum=carry+a.charAt(lena)-'0';
+            if(sum==2){
+                carry=1;
+                sb.insert(0,0);
+            }
+            else {
+                carry=0;
+                sb.insert(0,sum);
+            }
+            lena--;
+        }
+        while(lenb>=0){
+            int sum=carry+b.charAt(lenb)-'0';
+            if(sum==2){
+                carry=1;
+                sb.insert(0,0);
+            }
+            else {
+                carry=0;
+                sb.insert(0,sum);
+            }
+            lenb--;
+        }
+        if(carry==1) sb.insert(0,1);
+        return sb.toString();
     }
 }
