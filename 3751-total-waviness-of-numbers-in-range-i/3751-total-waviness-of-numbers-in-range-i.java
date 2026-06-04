946131c89f8b6c3635a5fd8f48e167101c1d04c7
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-04 23:26:44 +0530

    Time: 27 ms (59.11%), Space: 46.1 MB (80.67%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
@@ -2,15 +2,15 @@ class Solution {
     public int totalWaviness(int num1, int num2) {
         int count=0;
         for(int i=num1; i<=num2;i++){
-            List<Integer> list=new ArrayList<>();
-            int num=i;
-            while(num>0){
-                list.add(num%10);
-                num/=10;
-            }
-            if(list.size()<=2) continue;
-            for(int j=1;j<list.size()-1;j++){
-                if((list.get(j-1)<list.get(j) && list.get(j)>list.get(j+1)) || (list.get(j-1)>list.get(j) && list.get(j)<list.get(j+1)) ) count++;
+            String s = String.valueOf(i);
+
+            for(int j=1;j<s.length()-1;j++){
+                char a=s.charAt(j-1);
+                char b=s.charAt(j);
+                char c=s.charAt(j+1);
+
+                if((a<b && b>c) || (a>b && b<c))
+                    count++;
             }
         }
         return count;
