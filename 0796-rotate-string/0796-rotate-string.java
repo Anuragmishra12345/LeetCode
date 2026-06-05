df944b04a8d7f074daa47762b3ec0334346f6db9
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-05 18:20:10 +0530

    Time: 0 ms (100%), Space: 42.6 MB (72.45%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n=s.length();
+        if(goal.length()!=n) return false;
+
+        int right=0;
+        while(right<n){
+            if(s.charAt(right)==goal.charAt(0)){
+                int end=n-right;
+                String s1=s.substring(right,n);
+                String g1=goal.substring(0,end);
+
+                String s2=s.substring(0,right);
+                String g2=goal.substring(end,n);
+
+                if(s1.equals(g1) && s2.equals(g2)) return true;
+            }
+            right++;
+        }
+        return false;
+
+    }
+}
