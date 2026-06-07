4856af0586ad619158d4f8f3d794194016b1d861
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-08 01:52:06 +0530

    Time: 7 ms (28.39%), Space: 45.4 MB (88.87%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -0,0 +1,25 @@
+class Solution {
+    Integer[][] dp;
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        dp=new Integer[nums1.length][nums2.length];
+        return solve(nums1,nums2,0,0);
+    }
+    int solve(int[] nums1, int[] nums2, int i, int j){
+        if(i==nums1.length || j==nums2.length) return 0;
+
+        if(dp[i][j]!=null) return dp[i][j];
+
+        int ans=0;
+
+        if(nums1[i]==nums2[j]){
+            int take=1+solve(nums1,nums2,i+1,j+1);
+            int notTake=Math.max(solve(nums1,nums2,i,j+1),solve(nums1,nums2,i+1,j));
+
+            ans=Math.max(take,notTake);
+        }
+        else{
+            ans=Math.max(ans,Math.max(solve(nums1,nums2,i,j+1),solve(nums1,nums2,i+1,j)));
+        }
+        return dp[i][j]=ans;
+    }
+}
