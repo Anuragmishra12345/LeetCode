82367980f5558774c24d334b6025b3e4e143b616
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-12 23:00:24 +0530

    Time: 21 ms (8.51%), Space: 46.6 MB (23.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,36 @@
 class Solution {
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length()!=t.length()) return false;
+
+    //     char[] sc=s.toCharArray();
+    //     char[] tc=t.toCharArray();
+
+    //     Arrays.sort(sc);
+    //     Arrays.sort(tc);
+
+    //     for(int i=0;i<sc.length;i++){
+    //         if(sc[i]!=tc[i]) return false;
+    //     }
+    //     return true;
+    // }
+
+
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        
-        char[] sc=s.toCharArray();
-        char[] tc=t.toCharArray();
+        Map<Character,Integer> map=new HashMap();
 
-        Arrays.sort(sc);
-        Arrays.sort(tc);
-
-        for(int i=0;i<sc.length;i++){
-            if(sc[i]!=tc[i]) return false;
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)) return false;
+            else{
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)==0) map.remove(ch);
+            }
+        }
+        if(!map.isEmpty()) return false;
         return true;
+
     }
 }
