b144e539b8386617036167dbf0f1824f14a00d72
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-16 20:23:08 +0530

    Time: 17 ms (29.67%), Space: 57.5 MB (11.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,12 +1,12 @@
 class Solution {
-    // int[][] dp;
-    // public int change(int amount, int[] coins) {
-    //     int n=coins.length;
-    //     dp=new int[n][amount+1];
-    //     for(int[] row:dp) Arrays.fill(row,-1);
+    int[][] dp;
+    public int change(int amount, int[] coins) {
+        int n=coins.length;
+        dp=new int[n][amount+1];
+        for(int[] row:dp) Arrays.fill(row,-1);
 
-    //     return f(coins,amount,n-1);
-    // }
+        return dfs(amount,coins,n-1);
+    }
     // int f(int[] coins, int amt,int index){
     //     if(index==0){
     //         if(amt%coins[0]==0) return 1;
@@ -44,16 +44,28 @@ class Solution {
     //     return prev[amount];
     // }
 
-    public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
+    // public int change(int amount, int[] coins) {
+    //     int[] dp = new int[amount + 1];
+    //     dp[0] = 1;
 
-        for (int coin : coins) {
-            for (int t = coin; t <= amount; t++) {
-                dp[t] += dp[t - coin];
-            }
-        }
+    //     for (int coin : coins) {
+    //         for (int t = coin; t <= amount; t++) {
+    //             dp[t] += dp[t - coin];
+    //         }
+    //     }
 
-        return dp[amount];
+    //     return dp[amount];
+    // }
+
+    int dfs(int amount, int[] coins , int index){
+        if(amount==0) return 1;
+        if(amount<0 || index <0) return 0;
+
+        if(dp[index][amount]!=-1) return dp[index][amount];
+
+        int take=dfs(amount-coins[index],coins,index);
+        int notTake=dfs(amount,coins,index-1);
+
+        return dp[index][amount]=notTake+take;
     }
 }
