3b51beed4a0c4dd32dcfad37de88e4afa47e7423
Anurag Mishra <mishraanurag06542@gmail.com>
2026-06-23 01:29:30 +0530

    Time: 2 ms (54.43%), Space: 46.5 MB (47.48%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq=new int[26];
+
+        for(char ch:ransomNote.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(char ch:magazine.toCharArray()){
+            if(freq[ch-'a']>0) freq[ch-'a']--;
+        }
+
+        for(int f:freq){
+            if(f>0) return false;
+        }
+        return true;
+    }
+}
