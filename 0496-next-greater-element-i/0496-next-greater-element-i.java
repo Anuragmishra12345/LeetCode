26c30a46d1b96de65a7ab96acafce42ba7ca78d0
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-02 17:12:06 +0530

    Time: 3 ms (92.97%), Space: 45.5 MB (11.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -9,10 +9,10 @@ class Solution {
             }
             stack.push(num);
         }
-        int[] result=new int[nums1.length];
+        int[] ans=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            result[i]=map.getOrDefault(nums1[i],-1);
+            ans[i]=map.getOrDefault(nums1[i],-1);
         }
-        return result;
+        return ans;
     }
 }
