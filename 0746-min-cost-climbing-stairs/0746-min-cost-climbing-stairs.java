9a127a0347db07d739d38d92f0b5057537c25816
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-05 02:11:02 +0530

    Time: 0 ms (100%), Space: 44.9 MB (40.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
-    int[] dp;
-    public int minCostClimbingStairs(int[] cost) {
+    // int[] dp;
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n=cost.length;
+    //     dp=new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return Math.min(func(cost,0),func(cost,1));
+    // }
+    // int func(int[] cost, int index){
+    //     if(index>=cost.length) return 0;
+
+    //     if(dp[index]!=-1) return dp[index];
+
+    //     return dp[index]=cost[index]+Math.min(func(cost,index+1),func(cost,index+2));
+    // }
+
+    public int minCostClimbingStairs(int[] cost){
         int n=cost.length;
-        dp=new int[n];
-        Arrays.fill(dp,-1);
-        return Math.min(func(cost,0),func(cost,1));
-    }
-    int func(int[] cost, int index){
-        if(index>=cost.length) return 0;
+        int[] dp=new int[n+2];
+        dp[n]=0;
+        dp[n+1]=0;
 
-        if(dp[index]!=-1) return dp[index];
-
-        return dp[index]=cost[index]+Math.min(func(cost,index+1),func(cost,index+2));
+        for(int i=n-1;i>=0;i--){
+            dp[i]=cost[i]+Math.min(dp[i+1],dp[i+2]);
+        }
+        return Math.min(dp[0],dp[1]);
     }
 }
