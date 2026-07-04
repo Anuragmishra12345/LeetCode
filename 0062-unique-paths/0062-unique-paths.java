febcdbae8b0639a32e57a43fde891c3012ff2bf4
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-05 02:40:40 +0530

    Time: 54 ms (0.8%), Space: 42 MB (65.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,22 @@
 class Solution {
-    int r,c;
     int[][] dp;
     public int uniquePaths(int m, int n) {
         dp=new int[m][n];
-        for(int[]row: dp) Arrays.fill(row,-1);
-        r=m;c=n;
-        return dfs(0,0);
+        
+        for(int[] row:dp) Arrays.fill(row,-1);
+
+        return func(m,n,0,0);
     }
-    int dfs(int i, int j){
-        if(i==r-1 && j==c-1) return 1;
-        if(i>=r || j>=c) return 0;
+    int func(int m , int n, int i, int j){
+        if(i==m-1 && j==n-1) return 1;
+        
         if(dp[i][j]!=-1) return dp[i][j];
-        int right=dfs(i,j+1);
-        int down=dfs(i+1,j);
-        return dp[i][j]=right+down;
+
+        int bottom=0;
+        int right=0;
+        if(i+1<m) bottom=func(m,n,i+1,j);
+        if(j+1<n) right=func(m,n,i,j+1);
+
+        return dp[i][j]=bottom+right;
     }
 }
