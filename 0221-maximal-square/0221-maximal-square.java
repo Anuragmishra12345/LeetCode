42ac426562c303fa519cc377840d71699c3eb993
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-06 13:50:48 +0530

    Time: 11 ms (16.51%), Space: 70.7 MB (87.25%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,34 +1,21 @@
 class Solution {
-    int m;
-    int n;
-    int[][] dp;
     public int maximalSquare(char[][] matrix) {
-        m=matrix.length;
-        n=matrix[0].length;
-        dp=new int[m][n];
-        for(int[] row:dp) Arrays.fill(row,-1);
-    
-        int max=0;
-        for(int row=0;row<m;row++){
-            for(int col=0;col<n;col++){
-                if(matrix[row][col]=='1') {
-                    max=Math.max(max,solve(matrix,row,col));
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[][] dp=new int[m][n];
+        int maxArea=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int top=(i-1>=0)?dp[i-1][j]:0;
+                int left=(j-1>=0)?dp[i][j-1]:0;
+                int diag=(i-1>=0 && j-1>=0)?dp[i-1][j-1]:0;
+
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=1+Math.min(top,Math.min(left,diag));
+                    maxArea=Math.max(maxArea,dp[i][j]);
                 }
             }
         }
-        return max*max;
-    }
-
-    public int solve(char[][] matrix,int row, int col){
-        if(row>=m || col>=n) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        
-        if(matrix[row][col]=='0') return dp[row][col]=0;
-        
-        int right=solve(matrix,row,col+1);
-        int down=solve(matrix,row+1,col);
-        int diag=solve(matrix,row+1,col+1);
-
-        return dp[row][col]=1+Math.min(right,Math.min(down,diag));
+        return maxArea*maxArea;
     }
 }
