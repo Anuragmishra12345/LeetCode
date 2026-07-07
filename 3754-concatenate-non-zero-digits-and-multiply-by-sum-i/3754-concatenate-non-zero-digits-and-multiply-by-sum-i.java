fb927de58fcbd26a0e0918d50b233ec8c3fed647
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-08 00:44:32 +0530

    Time: 1 ms (99.85%), Space: 42.7 MB (52.17%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        String num=n+"";
+        String num=Integer.toString(n);
         long sum=0;
         long x=0;
         for(char ch:num.toCharArray()){
