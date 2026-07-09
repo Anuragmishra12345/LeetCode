1c112a4f54304c062ac6dbbe8271654e0a9948f0
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-09 09:40:28 +0530

    Time: 54 ms (42.45%), Space: 46.9 MB (92.43%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,24 +1,38 @@
 class Solution {
-    int[][] dp;
     public int longestPalindromeSubseq(String s) {
-        dp=new int[s.length()][s.length()];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        return func(s,0,s.length()-1);
+        return lcs(s,new StringBuilder(s).reverse().toString());
     }
-    int func(String s, int i , int j){
+    // int func(String s, int i , int j){
 
-        if(i>j) return 0;
+    //     if(i>j) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        int take=0;
+    //     int take=0;
 
-        if(s.charAt(i)==s.charAt(j)){
-            if(i!=j) take=2+func(s,i+1,j-1);
-            else take=1;
+    //     if(s.charAt(i)==s.charAt(j)){
+    //         if(i!=j) take=2+func(s,i+1,j-1);
+    //         else take=1;
+    //     }
+    //     int notTake=Math.max(func(s,i+1,j),func(s,i,j-1));
+
+    //     return dp[i][j]=Math.max(take,notTake);
+    // }
+
+    int lcs(String s1, String s2){
+        int n1=s1.length();
+        int n2=s2.length();
+
+        int[] dp=new int[n1+1];
+
+        for(int i=1;i<=n1;i++){
+            int[] currDp=new int[n1+1];
+            for(int j=1;j<=n1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) currDp[j]=1+dp[j-1];
+                else currDp[j]=Math.max(dp[j],currDp[j-1]);
+            }
+            dp=currDp;
         }
-        int notTake=Math.max(func(s,i+1,j),func(s,i,j-1));
-
-        return dp[i][j]=Math.max(take,notTake);
+        return dp[n1];
     }
 }
