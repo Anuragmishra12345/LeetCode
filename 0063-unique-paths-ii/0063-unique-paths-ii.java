18751bf455398737b8e408085a8cf92e52ca9174
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-10 13:11:55 +0530

    Time: 0 ms (100%), Space: 43.6 MB (45.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,26 @@
 class Solution {
-    int r,c;
+    int m;
+    int n;
+    int[][] dp;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        // if(obstacleGrid[m-1][n-1]==1) return 0;
-        r=m;
-        c=n;
-        int[][] dp=new int[m][n];
+        m=obstacleGrid.length;
+        n=obstacleGrid[0].length;
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        dp=new int[m][n];
         for(int[] row:dp) Arrays.fill(row,-1);
-        return dfs(obstacleGrid,0,0,dp);
+        return func(obstacleGrid,0,0);
     }
-    int dfs(int[][] grid, int i, int j, int[][] dp){
-        if(i>=r || j>=c || grid[i][j]==1) return 0;
-        if(i==r-1 && j==c-1) return 1;
-
+    int func(int[][] obs, int i , int j){
+        if(i==m-1 && j==n-1) return 1;
+        if(i>=m || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int right=dfs(grid,i,j+1,dp);
-        int down=dfs(grid,i+1,j,dp);
+
+        int right=0;
+        if(j+1<n && obs[i][j+1]!=1) right=func(obs, i ,j+1);
+
+        int down=0;
+        if(i+1<m && obs[i+1][j]!=1) down = func(obs,i+1,j);
+
         return dp[i][j]=right+down;
     }
 }
