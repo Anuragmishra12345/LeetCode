249decb24840007cc644fcb8bc25f353ed541504
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-10 11:16:35 +0530

    Time: 52 ms (24.42%), Space: 101.4 MB (38%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,43 +1,34 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, (a, b) -> {
-            if (a[0] == b[0])
-                return Integer.compare(b[1], a[1]);
-            return Integer.compare(a[0], b[0]);     
+        Arrays.sort(envelopes,(a,b)->{
+            if(a[0]==b[0]) return Integer.compare(b[1],a[1]);
+            return Integer.compare(a[0],b[0]);
         });
 
-        ArrayList<Integer> lis = new ArrayList<>();
+        ArrayList<Integer> lis=new ArrayList<>();
+        for(int[] env:envelopes){
+            int h=env[1];
 
-        for (int[] env : envelopes) {
+            int idx=lowerbound(lis,h);
 
-            int h = env[1];
-
-            int idx = lowerBound(lis, h);
-
-            if (idx == lis.size())
-                lis.add(h);
-            else
-                lis.set(idx, h);
+            if(idx==lis.size()) lis.add(h);
+            else lis.set(idx,h);
         }
-
         return lis.size();
     }
 
-    int lowerBound(ArrayList<Integer> list, int target) {
+    int lowerbound(ArrayList<Integer> list,int target){
+        int low=0;
+        int high=list.size();
 
-        int low = 0;
-        int high = list.size();
+        while(low<high){
+            int mid=low+(high-low)/2;
 
-        while (low < high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (list.get(mid) >= target)
-                high = mid;
-            else
-                low = mid + 1;
+            if(list.get(mid)>=target){
+                high=mid;
+            }
+            else low=mid+1;
         }
-
         return low;
     }
 }
