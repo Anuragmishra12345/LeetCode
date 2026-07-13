fd13b5c71009150a241891188bc1a721e0d685b4
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-13 17:46:31 +0530

    Time: 3 ms (87.85%), Space: 47.5 MB (69.1%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] arr=new int[ratings.length];
+        Arrays.fill(arr,1);
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]) arr[i]=arr[i-1]+1;
+        }
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1] && arr[i]<=arr[i+1]) arr[i]=arr[i+1]+1;
+        }
+
+        int candies=0;
+        for(int c:arr) candies+=c;
+        return candies;
+    }
+}
