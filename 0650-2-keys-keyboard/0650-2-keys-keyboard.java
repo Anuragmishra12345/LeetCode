2d0f0d5995ddb66a30e20f2afd8bc12ebd74375a
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-17 13:51:29 +0530

    Time: 22 ms (17.96%), Space: 70.3 MB (11.46%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[][] dp;
+    public int minSteps(int n) {
+        dp=new int[n+1][n+1];
+        for(int[] row:dp) Arrays.fill(row,-1);
+        return func(n,1,0);
+    }
+    int func(int n, int A, int c){
+        if(A==n) return 0;
+        if(A>n) return Integer.MAX_VALUE;
+
+        if(dp[A][c]!=-1) return dp[A][c]; 
+
+        int copy=func(n,A+A,A);
+        if(copy!=Integer.MAX_VALUE) copy+=2;
+
+        int paste=Integer.MAX_VALUE;
+        if(c!=0) paste=func(n,A+c,c);
+        if(paste!=Integer.MAX_VALUE) paste+=1;
+
+        return dp[A][c]=Math.min(copy,paste);
+    }
+}
