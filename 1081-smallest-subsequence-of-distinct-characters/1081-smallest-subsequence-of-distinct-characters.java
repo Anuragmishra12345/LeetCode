e10101b0f8db10a281d0c81bde3e32f81aa03dcd
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-19 10:58:37 +0530

    Time: 2 ms (98.45%), Space: 42.9 MB (47.01%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,29 +1,28 @@
 class Solution {
     public String smallestSubsequence(String s) {
-        Deque<Character> stack=new ArrayDeque<>();
         boolean[] visited=new boolean[26];
         int[] freq=new int[26];
 
+        for(char ch:s.toCharArray()) freq[ch-'a']++;
+        Deque<Character> stack=new ArrayDeque<>();
+
         for(char ch:s.toCharArray()){
-            freq[ch-'a']++;
-        }
-        for(char ch:s.toCharArray()){
-            if(visited[ch-'a']){
+            if(visited[ch-'a']) {
                 freq[ch-'a']--;
                 continue;
             }
-            while(!stack.isEmpty() && stack.peek()>ch && freq[stack.peek()-'a']>=1){
-                visited[stack.pop()-'a']=false;
+            while(!stack.isEmpty() && stack.peek()>ch && freq[stack.peek()-'a']>0){
+                visited[stack.peek()-'a']=false;
+                stack.pop();
             }
             stack.push(ch);
             freq[ch-'a']--;
             visited[ch-'a']=true;
         }
         StringBuilder result=new StringBuilder();
-        while(!stack.isEmpty()){
-            result.insert(0,stack.pop());
+        for(char ch:stack){
+            result.insert(0,ch);
         }
-
         return result.toString();
     }
 }
