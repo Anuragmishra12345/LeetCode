a42c3a7a2830f04e05c385670ccc8d3d586ded4d
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-22 13:06:55 +0530

    Time: 6 ms (68.58%), Space: 46.2 MB (49.09%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,18 +2,19 @@ class Solution {
     Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
         dp=new Boolean[s.length()];
-        Set<String> set=new HashSet<>();
-        for(String str:wordDict) set.add(str);
-
-        return backtrack(s,0,set);
+        Set<String> words=new HashSet<>();
+        for(String word:wordDict) words.add(word);
+        return func(s,words,0);
     }
-    boolean backtrack(String s, int index, Set<String> set){
-        if(index==s.length()) return true;
+    boolean func(String s, Set<String> words, int index){
+        if(s.length()==index) return true;
+
         if(dp[index]!=null) return dp[index];
-        for(int i=index;i<s.length();i++){
-            String curr=s.substring(index,i+1);
-            if(set.contains(curr)){
-                if(backtrack(s,i+1,set)) return dp[index]=true;
+
+        for(int i=index+1;i<=s.length();i++){
+            String word=s.substring(index,i);
+            if(words.contains(word)){
+                if(func(s,words,i)) return dp[index]=true;
             }
         }
         return dp[index]=false;
