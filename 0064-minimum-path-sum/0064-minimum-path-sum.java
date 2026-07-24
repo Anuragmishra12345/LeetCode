2ca8c8cc55b506110c051a97c99a0fff4903ee57
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-24 12:49:28 +0530

    Time: 1 ms (99.93%), Space: 50.2 MB (72.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,23 +1,22 @@
 class Solution {
     int[][] dp;
-    int r,c;
-    int min=Integer.MAX_VALUE;
     public int minPathSum(int[][] grid) {
-        r=grid.length;
-        c=grid[0].length;
-        dp=new int[r][c];
+        dp=new int[grid.length][grid[0].length];
         for(int[] row:dp) Arrays.fill(row,-1);
-        return dfs(grid,0,0);
+
+        return func(grid,0,0);
     }
-    int dfs(int[][] grid, int i,int j){
-        if(i>=r || j>=c) return Integer.MAX_VALUE;
-        if(i==r-1 && j==c-1) return grid[i][j];
+    int func(int[][] grid, int i, int j){
+        if(i==grid.length-1 && j==grid[0].length-1) return grid[i][j];
+        if(i>=grid.length || j>=grid[0].length) return Integer.MAX_VALUE;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        int right=dfs(grid,i,j+1);
-        int down=dfs(grid,i+1,j);
-        
-        return dp[i][j] = grid[i][j] + Math.min(right, down);
+        int down=func(grid,i+1,j);
+        if(down!=Integer.MAX_VALUE) down+=grid[i][j];
+        int right=func(grid,i,j+1);
+        if(right!=Integer.MAX_VALUE) right+=grid[i][j];
+
+        return dp[i][j]=Math.min(right,down);
     }
 }
