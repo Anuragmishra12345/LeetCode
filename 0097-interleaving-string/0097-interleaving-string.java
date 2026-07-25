b6cafebffd9390a992bdb5fc07a12cf5ce756d0b
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-25 20:17:27 +0530

    Time: 0 ms (100%), Space: 43.3 MB (39.84%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -3,21 +3,18 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length()) return false;
         dp=new Boolean[s1.length()+1][s2.length()+1];
-        return dfs(0,0,s1,s2,s3);
+        return solver(s1,s2,s3,0,0);
     }
-    private boolean dfs(int i, int j , String s1, String s2, String s3){
+    boolean solver(String s1, String s2 , String s3, int i, int j){
         if(i==s1.length() && j==s2.length()) return true;
 
         if(dp[i][j]!=null) return dp[i][j];
 
         int k=i+j;
-        boolean ans=false;
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
-            ans=ans|| dfs(i+1,j,s1,s2,s3);
-        }
-        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
-            ans=ans||dfs(i,j+1,s1,s2,s3);
-        }
+        boolean ans = false;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)) ans=solver(s1,s2,s3,i+1,j);
+
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)) ans=ans || solver(s1,s2,s3,i,j+1);
 
         return dp[i][j]=ans;
     }
