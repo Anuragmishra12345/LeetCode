c822d4534455f03df5a5ce28d12c90a24ba14872
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-25 13:38:22 +0530

    Time: 0 ms (100%), Space: 45.6 MB (5.56%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,23 +1,20 @@
 class Solution {
     int[][] dp;
-    int m,n;
     public int calculateMinimumHP(int[][] dungeon) {
-        m=dungeon.length;
-        n=dungeon[0].length;
-        dp=new int [m][n];
-        for(int[] row: dp) Arrays.fill(row,Integer.MIN_VALUE);
-        return f(dungeon,0,0);
+        dp=new int[dungeon.length][dungeon[0].length];
+        for(int[] row:dp) Arrays.fill(row,Integer.MIN_VALUE);
+        return func(dungeon,0,0);
     }
-    int f(int[][] grid,int i, int j){
-        if(i==m-1 && j==n-1 ) return Math.max(1,1-grid[i][j]);
-        if(i>=m || j>=n) return Integer.MAX_VALUE;
+    int func(int[][] grid, int i, int j){
+        if(i==grid.length-1 && j==grid[0].length-1) return Math.max(1,1-grid[i][j]);
+        if(i>=grid.length || j>=grid[0].length) return Integer.MAX_VALUE;
 
         if(dp[i][j]!=Integer.MIN_VALUE) return dp[i][j];
 
-        int down=f(grid,i+1,j);
-        int right=f(grid,i,j+1);
+        int right=func(grid,i,j+1);
+        int down=func(grid,i+1,j);
 
-        int need=Math.min(down,right);
+        int need=Math.min(right,down);
 
         return dp[i][j]=Math.max(1,need-grid[i][j]);
     }
