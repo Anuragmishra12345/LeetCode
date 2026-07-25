1b50a42ddab7e3e9d990c3875fb4e68aba8c02fb
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-25 19:51:26 +0530

    Time: 2 ms (73.2%), Space: 45.3 MB (56.15%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,21 +1,42 @@
 class Solution {
-    int[][] dp;
+    // int[][] dp;
+    // public int calculateMinimumHP(int[][] dungeon) {
+    //     dp=new int[dungeon.length][dungeon[0].length];
+    //     for(int[] row:dp) Arrays.fill(row,Integer.MIN_VALUE);
+    //     return func(dungeon,0,0);
+    // }
+    // int func(int[][] grid, int i, int j){
+    //     if(i==grid.length-1 && j==grid[0].length-1) return Math.max(1,1-grid[i][j]);
+    //     if(i>=grid.length || j>=grid[0].length) return Integer.MAX_VALUE;
+
+    //     if(dp[i][j]!=Integer.MIN_VALUE) return dp[i][j];
+
+    //     int right=func(grid,i,j+1);
+    //     int down=func(grid,i+1,j);
+
+    //     int need=Math.min(right,down);
+
+    //     return dp[i][j]=Math.max(1,need-grid[i][j]);
+    // }
+
     public int calculateMinimumHP(int[][] dungeon) {
-        dp=new int[dungeon.length][dungeon[0].length];
-        for(int[] row:dp) Arrays.fill(row,Integer.MIN_VALUE);
-        return func(dungeon,0,0);
-    }
-    int func(int[][] grid, int i, int j){
-        if(i==grid.length-1 && j==grid[0].length-1) return Math.max(1,1-grid[i][j]);
-        if(i>=grid.length || j>=grid[0].length) return Integer.MAX_VALUE;
+        int m=dungeon.length;
+        int n=dungeon[0].length;
+        int[][] dp=new int[m+1][n+1];
+        for (int[] row : dp) Arrays.fill(row, Integer.MAX_VALUE);
+                        
+        dp[m][n-1]=1;
+        dp[m-1][n]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int right=dp[i][j+1];
+                int down=dp[i+1][j];
 
-        if(dp[i][j]!=Integer.MIN_VALUE) return dp[i][j];
+                int need=Math.min(right,down);
 
-        int right=func(grid,i,j+1);
-        int down=func(grid,i+1,j);
-
-        int need=Math.min(right,down);
-
-        return dp[i][j]=Math.max(1,need-grid[i][j]);
+                dp[i][j]=Math.max(1,need-dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
     }
 }
