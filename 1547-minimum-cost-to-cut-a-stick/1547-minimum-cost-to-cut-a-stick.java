d6ac239d88ea2313b943cc105b87dd8a4a0fd41e
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-26 23:21:48 +0530

    Time: 14 ms (95.61%), Space: 44.1 MB (96.89%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,28 +1,54 @@
 class Solution {
-    int[][] dp;
+    // int[][] dp;
+    // public int minCost(int n, int[] cuts) {
+    //     int c=cuts.length;
+    //     int[] arr=new int[c+2];
+
+    //     dp=new int[c+2][c+2];
+    //     for(int[] row:dp) Arrays.fill(row,-1);
+
+    //     for(int i=0;i<c;i++){
+    //         arr[i+1]=cuts[i];
+    //     }
+    //     arr[c+1]=n;
+    //     Arrays.sort(arr);
+    //     return func(1,c,arr);
+    // }
+    // int func(int i,int j ,int[] cuts){
+    //     if(i>j) return 0;
+
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int cost=Integer.MAX_VALUE;
+    //     for(int k=i;k<=j;k++){
+    //         int currCost=cuts[j+1]-cuts[i-1]+func(i,k-1,cuts)+func(k+1,j,cuts);
+    //         cost=Math.min(cost,currCost);
+    //     }
+    //     return dp[i][j]=cost;
+    // }
+
     public int minCost(int n, int[] cuts) {
         int c=cuts.length;
         int[] arr=new int[c+2];
 
-        dp=new int[c+2][c+2];
-        for(int[] row:dp) Arrays.fill(row,-1);
+        int[][] dp=new int[c+2][c+2];
 
         for(int i=0;i<c;i++){
             arr[i+1]=cuts[i];
         }
         arr[c+1]=n;
         Arrays.sort(arr);
-        return func(1,c,arr);
-    }
-    int func(int i,int j ,int[] cuts){
-        if(i>j) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        int cost=Integer.MAX_VALUE;
-        for(int k=i;k<=j;k++){
-            int currCost=cuts[j+1]-cuts[i-1]+func(i,k-1,cuts)+func(k+1,j,cuts);
-            cost=Math.min(cost,currCost);
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+                int cost=Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    int currCost=arr[j+1]-arr[i-1]+ dp[i][k-1]+dp[k+1][j];
+                    cost=Math.min(cost,currCost);
+                }
+                dp[i][j]=cost;
+            }
         }
-        return dp[i][j]=cost;
+        return dp[1][c];
     }
 }
