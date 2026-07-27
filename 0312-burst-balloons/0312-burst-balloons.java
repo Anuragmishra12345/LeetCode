234bdd62bf691c3e8871c8fefe3328aa3018e8db
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-28 01:07:57 +0530

    Time: 41 ms (88.23%), Space: 44.6 MB (33.11%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,27 +1,51 @@
 class Solution {
-    int[][] dp;
+    // int[][] dp;
+    // public int maxCoins(int[] nums) {
+    //     int n=nums.length;
+    //     dp=new int[n+2][n+2];
+    //     for(int[] row:dp) Arrays.fill(row,-1);
+    //     int[] arr=new int[n+2];
+    //     arr[0]=1;
+    //     arr[n+1]=1;
+    //     for(int i=0;i<n;i++){
+    //         arr[i+1]=nums[i];
+    //     }
+    //     return func(arr,1,n);
+    // }
+    // int func(int[] nums, int i, int j){
+    //     if(i>j) return 0;
+
+    //     if(dp[i][j]!=-1) return dp[i][j];
+
+    //     int coins=0;
+    //     for(int k=i;k<=j;k++){
+    //         int c=nums[i-1]*nums[k]*nums[j+1] +func(nums,i,k-1)+func(nums,k+1,j);
+    //         coins=Math.max(coins,c);
+    //     }
+    //     return dp[i][j]=coins;
+    // }
+
+
     public int maxCoins(int[] nums) {
         int n=nums.length;
-        dp=new int[n+2][n+2];
-        for(int[] row:dp) Arrays.fill(row,-1);
+        int[][] dp=new int[n+2][n+2];
         int[] arr=new int[n+2];
         arr[0]=1;
         arr[n+1]=1;
         for(int i=0;i<n;i++){
             arr[i+1]=nums[i];
         }
-        return func(arr,1,n);
-    }
-    int func(int[] nums, int i, int j){
-        if(i>j) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int coins=0;
-        for(int k=i;k<=j;k++){
-            int c=nums[i-1]*nums[k]*nums[j+1] +func(nums,i,k-1)+func(nums,k+1,j);
-            coins=Math.max(coins,c);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                int cost=0;
+                for(int k=i;k<=j;k++){
+                    int c=arr[i-1]*arr[k]*arr[j+1]+dp[i][k-1]+dp[k+1][j];
+                    cost=Math.max(cost,c);
+                }
+                dp[i][j]=cost;
+            }
         }
-        return dp[i][j]=coins;
+        return dp[1][n];
     }
 }
