793dfc5860bdaf6d6b2714ffe9445baf7a820be0
Anurag Mishra <mishraanurag06542@gmail.com>
2026-07-28 00:56:13 +0530

    Time: 112 ms (39.54%), Space: 44.1 MB (92.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,48 +1,27 @@
 class Solution {
-    // int[][] dp;
-    // public int maxCoins(int[] nums) {
-    //     int n=nums.length;
-    //     dp=new int[n+2][n+2];
-    //     for(int[] row:dp) Arrays.fill(row,-1);
-    //     int[] arr=new int[n+2];
-    //     arr[0]=1;
-    //     arr[n+1]=1;
-    //     for(int i=0;i<n;i++){
-    //         arr[i+1]=nums[i];
-    //     }
-    //     return f(1,n,arr);
-    // }
-    // int f(int i, int j, int[] nums){
-    //     if(i>j) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int max=Integer.MIN_VALUE;
-    //     for(int k=i;k<=j;k++){
-    //         int coin=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums)+f(k+1,j,nums);
-    //         max=Math.max(coin,max);
-    //     }
-    //     return dp[i][j]=max;
-    // }
-
+    int[][] dp;
     public int maxCoins(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n+2][n+2];
+        dp=new int[n+2][n+2];
+        for(int[] row:dp) Arrays.fill(row,-1);
         int[] arr=new int[n+2];
         arr[0]=1;
         arr[n+1]=1;
         for(int i=0;i<n;i++){
             arr[i+1]=nums[i];
         }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n;j++){
-                if(i>j) continue;
-                int max=Integer.MIN_VALUE;
-                for(int k=i;k<=j;k++){
-                    int coin=arr[i-1]*arr[k]*arr[j+1]+dp[i][k-1]+dp[k+1][j];
-                    max=Math.max(coin,max);
-                }
-                dp[i][j]=max;
-            }
+        return func(arr,1,n);
+    }
+    int func(int[] nums, int i, int j){
+        if(i>j) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int coins=0;
+        for(int k=i;k<=j;k++){
+            int c=nums[i-1]*nums[k]*nums[j+1] +func(nums,i,k-1)+func(nums,k+1,j);
+            coins=Math.max(coins,c);
         }
-        return dp[1][n];
+        return dp[i][j]=coins;
     }
 }
