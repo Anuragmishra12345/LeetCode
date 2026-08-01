5b898236faa9323b1f18c3cd66fd32163ee96428
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-02 00:33:17 +0530

    Time: 13 ms (74.22%), Space: 46.6 MB (65.47%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,23 +1,32 @@
 class Solution {
-    public String frequencySort(String s) {
-        Map<Character, Integer> map=new HashMap<>();
-        for(char ch:s.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
+    class Pair{
+        char c;
+        int v;
 
-        List<Character>[] bucket=new  ArrayList[s.length()+1];
-        for(Map.Entry<Character, Integer> entry: map.entrySet() ){
-            char key=entry.getKey();
-            int value=entry.getValue();
-            if(bucket[value]==null) bucket[value]=new ArrayList<>();
-            bucket[value].add(key);
+        Pair(char c,int v){
+            this.c=c;
+            this.v=v;
         }
+    }
+    public String frequencySort(String s) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Integer.compare(b.v,a.v));
+
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            pq.offer(new Pair(entry.getKey(),entry.getValue()));
+        }
+
         StringBuilder result=new StringBuilder();
-        for(int i=s.length();i>0 ;i--){
-            if(bucket[i]==null) continue;
-            for(char ch:bucket[i]){
-                for(int j=0;j<i;j++){
-                    result.append(ch);
-                }
-            }
+
+        while(!pq.isEmpty()){
+            char ch=pq.peek().c;
+            int value=pq.poll().v;
+
+            for(int i=0;i<value;i++) result.append(ch);
         }
         return result.toString();
     }
