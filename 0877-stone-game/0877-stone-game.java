c87a1cf41f0bad67479882d0b6d8eb1d2feaa1c1
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-02 19:42:58 +0530

    Time: 6 ms (22.55%), Space: 46.6 MB (21.03%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    // Integer[][] dp;
+    public boolean stoneGame(int[] piles) {
+        // dp=new Integer[piles.length][piles.length];
+
+        int[][] dp=new int[piles.length+1][piles.length+1];
+        for(int i=0;i<piles.length;i++){
+            dp[i][i]=piles[i];
+        }
+
+        for(int i=piles.length-1;i>=0;i--){
+            for(int j=i+1;j<piles.length;j++){
+                
+                int left=piles[i]-dp[i+1][j];
+                int right=piles[j]-dp[i][j-1];
+
+                dp[i][j]=Math.max(left,right);
+            }
+        }
+
+        return dp[0][piles.length-1]>0;
+        // return func(piles,0,piles.length-1)>=0;
+    }
+    // int func(int[] piles, int i, int j){
+    //     if(i==j) return piles[i];
+
+    //     if(dp[i][j]!=null) return dp[i][j];
+
+    //     int left=piles[i]-func(piles,i+1,j);
+    //     int right=piles[j]-func(piles,i,j-1);
+
+    //     return dp[i][j]=Math.max(left,right);
+    // }
+}
