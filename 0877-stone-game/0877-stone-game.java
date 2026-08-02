3baa0dcbabd734aabafb4edb4eafd8e4678f374e
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-02 19:45:16 +0530

    Time: 0 ms (100%), Space: 43.2 MB (48.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,26 +1,10 @@
 class Solution {
     // Integer[][] dp;
-    public boolean stoneGame(int[] piles) {
-        // dp=new Integer[piles.length][piles.length];
+    // public boolean stoneGame(int[] piles) {
+    //     dp=new Integer[piles.length][piles.length];
 
-        int[][] dp=new int[piles.length+1][piles.length+1];
-        for(int i=0;i<piles.length;i++){
-            dp[i][i]=piles[i];
-        }
-
-        for(int i=piles.length-1;i>=0;i--){
-            for(int j=i+1;j<piles.length;j++){
-                
-                int left=piles[i]-dp[i+1][j];
-                int right=piles[j]-dp[i][j-1];
-
-                dp[i][j]=Math.max(left,right);
-            }
-        }
-
-        return dp[0][piles.length-1]>0;
-        // return func(piles,0,piles.length-1)>=0;
-    }
+    //     return func(piles,0,piles.length-1)>=0;
+    // }
     // int func(int[] piles, int i, int j){
     //     if(i==j) return piles[i];
 
@@ -31,4 +15,26 @@ class Solution {
 
     //     return dp[i][j]=Math.max(left,right);
     // }
+
+    // public boolean stoneGame(int[] piles){
+    //     int[][] dp=new int[piles.length+1][piles.length+1];
+    //     for(int i=0;i<piles.length;i++){
+    //         dp[i][i]=piles[i];
+    //     }
+
+    //     for(int i=piles.length-1;i>=0;i--){
+    //         for(int j=i+1;j<piles.length;j++){
+                
+    //             int left=piles[i]-dp[i+1][j];
+    //             int right=piles[j]-dp[i][j-1];
+
+    //             dp[i][j]=Math.max(left,right);
+    //         }
+    //     }
+
+    //     return dp[0][piles.length-1]>0;
+    
+    // }
+
+    public boolean stoneGame(int[] piles){ return true;}
 }
