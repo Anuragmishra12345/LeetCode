e3ab20108f4f660f41882d8769eb56af12a9376e
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-09 21:17:05 +0530

    Time: 19 ms (24.36%), Space: 47.3 MB (86.6%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,43 +1,46 @@
 class Solution {
-    class Tuple{
-        int first;
-        int second;
-        int third;
+    class Triple{
+        int dist;
+        int i;
+        int j;
 
-        Tuple(int first, int second, int third){
-            this.first=first;
-            this.second=second;
-            this.third=third;
+        Triple(int dist,int i, int j){
+            this.dist=dist;
+            this.i=i;
+            this.j=j;
         }
     }
-    int[][] dir={{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
+    int[][] dir={{0,1},{1,0},{1,1},{-1,-1},{-1,0},{0,-1},{1,-1},{-1,1}};
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n=grid.length;
         if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+
+        Queue<Triple> q=new LinkedList<>();
+
         int[][] path=new int[n][n];
-        for(int[] row:path) Arrays.fill(row,Integer.MAX_VALUE);
+        for(int[] p:path) Arrays.fill(p,Integer.MAX_VALUE);
+
         path[0][0]=1;
-        Queue<Tuple> q=new LinkedList<>();
-        q.add(new Tuple(1,0,0));
+
+        q.add(new Triple(1,0,0));
 
         while(!q.isEmpty()){
-            Tuple it=q.poll();
-            int dist=it.first;
-            int r=it.second;
-            int c=it.third;
+            Triple t=q.poll();
             
-            if(dist>path[r][c]) continue;
+            int dist=t.dist;
+            int r=t.i;
+            int c=t.j;
 
-            for(int i=0;i<8;i++){
-                int nr=r+dir[i][0];
-                int nc=c+dir[i][1];
-            
-                if(nr>=0 && nr<n && nc<n && nc>=0 && grid[nr][nc]==0 && dist+1<path[nr][nc]){
+            for(int[] d:dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==0 && dist+1<path[nr][nc]){
                     path[nr][nc]=dist+1;
-                    q.add(new Tuple(dist+1,nr,nc));
+                    q.offer(new Triple(dist+1,nr,nc));
                 }
             }
         }
-        return (path[n-1][n-1]!=Integer.MAX_VALUE)? path[n-1][n-1]:-1;
+        return path[n-1][n-1]==Integer.MAX_VALUE?-1:path[n-1][n-1];
     }
 }
