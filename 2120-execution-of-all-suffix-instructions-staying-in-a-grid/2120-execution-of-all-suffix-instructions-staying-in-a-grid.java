36d129c4e82fcfb664cc9930fc4e6beed814991a
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-14 17:44:04 +0530

    Time: 22 ms (85.25%), Space: 46.7 MB (43.17%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -1,30 +1,34 @@
 class Solution {
     public int[] executeInstructions(int n, int[] startPos, String s) {
-        int r=startPos[0];
-        int c=startPos[1];
-        int m=s.length();
 
-        int[] ans=new int[m];
-        for(int i=0;i<m;i++){
-            ans[i]=dfs(n,r,c,i,s);
+        int m = s.length();
+        int[] ans = new int[m];
+
+        for (int start = 0; start < m; start++) {
+
+            int r = startPos[0];
+            int c = startPos[1];
+
+            int count = 0;
+
+            for (int i = start; i < m; i++) {
+
+                char ch = s.charAt(i);
+
+                if (ch == 'R') c++;
+                else if (ch == 'L') c--;
+                else if (ch == 'U') r--;
+                else r++;
+
+                if (r < 0 || r >= n || c < 0 || c >= n)
+                    break;
+
+                count++;
+            }
+
+            ans[start] = count;
         }
+
         return ans;
-    }
-    int dfs(int n, int i, int j, int index, String s){
-        if(index==s.length()) return 0;
-        char dir=s.charAt(index);
-        int r=i;
-        int c=j;
-
-        if(dir=='R') c++;
-        else if(dir=='L') c--;
-        else if(dir=='U') r--;
-        else r++;
-
-        if(r>=n || c>=n || r<0 || c<0 ) return 0;
-
-        int step=1+dfs(n,r,c,index+1,s);
-
-        return step;
     }
 }
