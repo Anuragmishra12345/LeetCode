99c6a2abc954e2592178ef38b688d80770645a29
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-15 11:57:54 +0530

    Time: 2309 ms (27.82%), Space: 42.6 MB (88.17%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,48 +1,32 @@
 class Solution {
     int[] dp;
+    public int minCut(String s) {
+        dp=new int[s.length()];
+        Arrays.fill(dp,-1);
+        return func(s,0)-1;
+    }
+    int func(String s, int i){
+        if(i>=s.length()) return 0;
 
-    // public int minCut(String s) {
-    //     dp=new int[s.length()];
-    //     Arrays.fill(dp,-1);
-    //     return f(s,0)-1;
-    // }
-    // int f(String s,int i){
-    //     if(i>=s.length()) return 0;
-    //     int minCost=Integer.MAX_VALUE;
-    //     if(dp[i]!=-1) return dp[i];
-    //     for(int j=i;j<s.length();j++){
-            // if(isPalindrome(s,i,j)){
-            //     int cost=1+f(s,j+1);
-            //     minCost=Math.min(minCost,cost);
-            // }
-    //     }
-    //     return dp[i]=minCost;
-    // }
+        if(dp[i]!=-1) return dp[i];
 
-    boolean isPalindrome(String s, int start, int end){
-        while(start<=end){
-            if(s.charAt(start)!=s.charAt(end)) return false;
-            start++;
-            end--;
+        int cost=Integer.MAX_VALUE;
+
+        for(int j=i;j<s.length();j++){
+            if(isPalindrome(s,i,j)){
+                int c=1+func(s,j+1);
+                cost=Math.min(cost,c);
+            }
+        }
+        return dp[i]=cost;
+    }
+
+    boolean isPalindrome(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
         }
         return true;
     }
-
-    public int minCut(String s) {
-        int n=s.length();
-        dp=new int[n+1];
-        
-        for(int i=n-1;i>=0;i--){
-            int minCost=Integer.MAX_VALUE;
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
-                    int cost=1+dp[j+1];
-                    minCost=Math.min(minCost,cost);
-                }
-            }
-            dp[i]=minCost;
-            
-        }
-        return dp[0]-1;
-    }
-} 
+}
