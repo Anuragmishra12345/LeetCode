54855318fe6ef4f8b8fb27954e6e43a2ce96da5a
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-15 12:04:02 +0530

    Time: 2292 ms (31.16%), Space: 42.7 MB (81.81%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,25 +1,25 @@
 class Solution {
-    int[] dp;
-    public int minCut(String s) {
-        dp=new int[s.length()];
-        Arrays.fill(dp,-1);
-        return func(s,0)-1;
-    }
-    int func(String s, int i){
-        if(i>=s.length()) return 0;
+    // int[] dp;
+    // public int minCut(String s) {
+    //     dp=new int[s.length()];
+    //     Arrays.fill(dp,-1);
+    //     return func(s,0)-1;
+    // }
+    // int func(String s, int i){
+    //     if(i>=s.length()) return 0;
 
-        if(dp[i]!=-1) return dp[i];
+    //     if(dp[i]!=-1) return dp[i];
 
-        int cost=Integer.MAX_VALUE;
+    //     int cost=Integer.MAX_VALUE;
 
-        for(int j=i;j<s.length();j++){
-            if(isPalindrome(s,i,j)){
-                int c=1+func(s,j+1);
-                cost=Math.min(cost,c);
-            }
-        }
-        return dp[i]=cost;
-    }
+    //     for(int j=i;j<s.length();j++){
+    //         if(isPalindrome(s,i,j)){
+    //             int c=1+func(s,j+1);
+    //             cost=Math.min(cost,c);
+    //         }
+    //     }
+    //     return dp[i]=cost;
+    // }
 
     boolean isPalindrome(String s, int i, int j){
         while(i<j){
@@ -28,5 +28,23 @@ class Solution {
             j--;
         }
         return true;
+    }
+
+    public int minCut(String s) {
+        int n=s.length();
+        int[] dp=new int[n+1];
+
+        for(int i=n-1;i>=0;i--){
+            int cost=Integer.MAX_VALUE;
+
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)){
+                    int c=1+dp[j+1];
+                    cost=Math.min(cost,c);
+                }
+            }
+            dp[i]=cost;
+        }
+        return dp[0]-1;
     }
 }
