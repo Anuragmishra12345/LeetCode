247e71108f4722d708a05845bd600b5e32111ca9
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-24 12:24:06 +0530

    Time: 4 ms (66.81%), Space: 91.8 MB (43.4%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,21 +1,35 @@
 class Solution {
-    Integer[] dp;
+    // Integer[] dp;
+    // public int stoneGameVIII(int[] stones) {
+    //     dp=new Integer[stones.length];
+
+    //     for(int i=1;i<stones.length;i++){
+    //         stones[i]=stones[i]+stones[i-1];
+    //     }
+
+    //     return func(stones,1);
+    // }
+    //  private int func(int[] prefix, int i) {
+    //     if(i==prefix.length-1) return prefix[i];
+
+    //     if(dp[i]!=null)  return dp[i];
+    //     int take=prefix[i]-func(prefix,i+1);
+    //     int notTake=func(prefix,i+1);
+
+    //     return dp[i]=Math.max(take,notTake);
+    // }
+
     public int stoneGameVIII(int[] stones) {
-        dp=new Integer[stones.length];
+        int[] dp=new int[stones.length];
 
         for(int i=1;i<stones.length;i++){
             stones[i]=stones[i]+stones[i-1];
         }
+        dp[stones.length-1]=stones[stones.length-1];
 
-        return func(stones,1);
-    }
-     private int func(int[] prefix, int i) {
-        if(i==prefix.length-1) return prefix[i];
-
-        if(dp[i]!=null)  return dp[i];
-        int take=prefix[i]-func(prefix,i+1);
-        int notTake=func(prefix,i+1);
-
-        return dp[i]=Math.max(take,notTake);
+        for(int i=stones.length-2;i>=1;i--){
+            dp[i]=Math.max(stones[i]-dp[i+1],dp[i+1]);
+        }
+        return dp[1];
     }
 }
