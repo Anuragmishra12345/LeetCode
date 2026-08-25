76cb607886360438f575e89d7fbbfd894344da3c
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-25 22:09:43 +0530

    Time: 2324 ms (25.74%), Space: 42.5 MB (93.52%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -21,40 +21,71 @@ class Solution {
     //     return dp[i]=cost;
     // }
 
-    boolean isPalindrome(String s, int i, int j){
+    // boolean isPalindrome(String s, int i, int j){
+    //     while(i<j){
+    //         if(s.charAt(i)!=s.charAt(j)) return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+
+    // public int minCut(String s) {
+    //     int n=s.length();
+    //     int[] dp=new int[n+1];
+
+    //     boolean[][] palindrome=new boolean[n][n];
+
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=i;j<n;j++){
+    //             if(s.charAt(i)==s.charAt(j) && (j-i<=1 || palindrome[i+1][j-1])){
+    //                 palindrome[i][j]=true;
+    //             }
+    //         }
+    //     }
+
+    //     for(int i=n-1;i>=0;i--){
+    //         int cost=Integer.MAX_VALUE;
+
+    //         for(int j=i;j<n;j++){
+    //             if(palindrome[i][j]){
+    //                 int c=1+dp[j+1];
+    //                 cost=Math.min(cost,c);
+    //             }
+    //         }
+    //         dp[i]=cost;
+    //     }
+    //     return dp[0]-1;
+    // }
+
+    Integer[] dp;
+
+    public int minCut(String s) {
+        dp=new Integer[s.length()];
+        return func(s,0)-1;
+    }
+
+    int func(String s, int i){
+        if(i==s.length()) return 0;
+
+        if(dp[i]!=null) return dp[i];
+
+        int cuts=Integer.MAX_VALUE;
+
+        for(int k=i;k<s.length();k++){
+            if(isPalindrome(s,i,k)){
+                cuts=Math.min(cuts,1+func(s,k+1));
+            }
+        }
+        return dp[i]=cuts;
+    }
+
+    boolean isPalindrome(String s, int i , int j){
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
             j--;
         }
         return true;
-    }
-
-    public int minCut(String s) {
-        int n=s.length();
-        int[] dp=new int[n+1];
-
-        boolean[][] palindrome=new boolean[n][n];
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
-                if(s.charAt(i)==s.charAt(j) && (j-i<=1 || palindrome[i+1][j-1])){
-                    palindrome[i][j]=true;
-                }
-            }
-        }
-
-        for(int i=n-1;i>=0;i--){
-            int cost=Integer.MAX_VALUE;
-
-            for(int j=i;j<n;j++){
-                if(palindrome[i][j]){
-                    int c=1+dp[j+1];
-                    cost=Math.min(cost,c);
-                }
-            }
-            dp[i]=cost;
-        }
-        return dp[0]-1;
     }
 }
