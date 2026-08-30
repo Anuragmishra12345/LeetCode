06e389c2147d0cb02bb48e9510a2f39ce7a039c8
Anurag Mishra <mishraanurag06542@gmail.com>
2026-08-30 17:12:22 +0530

    Time: 24 ms (10.42%), Space: 43 MB (12.22%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
@@ -1,17 +1,29 @@
 class Solution {
-    int[] dp;
+    // int[] dp;
+    // public int minCost(int n) {
+    //     dp=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return func(n);
+    // }
+    // private int func(int n){
+    //     if(n==1) return 0; 
+    //     if(dp[n]!=-1) return dp[n];
+    //     int cost=Integer.MAX_VALUE;
+    //     for(int i=1;i<=n/2;i++){
+    //         cost=Math.min(cost,i*(n-i)+func(i)+func(n-i));
+    //     }
+    //     return dp[n]=cost;
+    // }
+
     public int minCost(int n) {
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return func(n);
-    }
-    private int func(int n){
-        if(n==1) return 0; 
-        if(dp[n]!=-1) return dp[n];
-        int cost=Integer.MAX_VALUE;
-        for(int i=1;i<=n/2;i++){
-            cost=Math.min(cost,i*(n-i)+func(i)+func(n-i));
+        int[] dp=new int[n+1];
+        for(int j = 2; j <= n; j++){
+            int cost=Integer.MAX_VALUE;
+            for(int i=1;i<=j/2;i++){
+                cost=Math.min(cost,i*(j-i)+dp[i]+dp[j-i]);
+            }
+            dp[j]=cost;
         }
-        return dp[n]=cost;
+        return dp[n];
     }
 }
