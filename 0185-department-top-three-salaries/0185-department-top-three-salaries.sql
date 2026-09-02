471648298832849162eb1464a7a5a88f745aa708
Anurag Mishra <mishraanurag06542@gmail.com>
2026-09-02 21:26:10 +0530

    Time: 1414 ms (22.86%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,10 +1,9 @@
-select d.name as Department , e.name as Employee , e.salary as Salary
+select d.name as Department, e.name as Employee , e.salary as Salary
 from Employee e
 join Department d
 on e.departmentId=d.id
 where 3>(
-    select count(distinct e2.salary)
-    from Employee e2
-    where e2.departmentId=e.departmentId
-    and e2.salary>e.salary
+    select count(distinct e1.salary)
+    from Employee e1
+    where e1.departmentId=e.departmentId && e1.salary>e.salary
 );
