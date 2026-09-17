a623b153e760182093789974e59d3c92e29bd72a
Anurag Mishra <mishraanurag06542@gmail.com>
2026-09-17 14:30:18 +0530

    Time: 4 ms (91.46%), Space: 47.2 MB (48.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,39 @@
 class Solution {
     boolean[] visited;
     boolean[] path;
-    public boolean canFinish(int numCourses, int[][] pr) {
-        int n=pr.length;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         visited=new boolean[numCourses];
         path=new boolean[numCourses];
 
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<>());
+        List<List<Integer>> adjList=new ArrayList<>();
+        for(int i=0;i<numCourses;i++) adjList.add(new ArrayList<>());
+
+        for(int[] p:prerequisites){
+            int a=p[0];
+            int b=p[1];
+
+            adjList.get(b).add(a);
         }
 
-        for(int[] e : pr){
-            int a = e[0];
-            int b = e[1];
-            adj.get(b).add(a);
-        }
         for(int i=0;i<numCourses;i++){
             if(!visited[i]){
-                if(dfs(i,adj)) return false;
+                if(dfs(i,adjList)) return false;
             }
         }
         return true;
     }
 
-    boolean dfs(int node, List<List<Integer>> adj){
+    boolean dfs(int node, List<List<Integer>> adjList){
         visited[node]=true;
         path[node]=true;
 
-        for(int n:adj.get(node)){
-            if(!visited[n]) {
-                if(dfs(n,adj)) return true;
+        for(int n:adjList.get(node)){
+            if(!visited[n]){
+                if(dfs(n,adjList)) return true;
             }
             else if(path[n]) return true;
         }
+
 
         path[node]=false;
         return false;
